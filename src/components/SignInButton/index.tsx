1975c645b8c07a256f38681887f0ce987f986307
Barreiradlc <augustodasilva53@gmail.com>
2021-06-28 22:42:39 -0300

    Erro em checkout

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,22 @@
 import styles from './styles.module.scss'
 import { FaGithub } from 'react-icons/fa'
+import { signIn, useSession, signOut } from "next-auth/client"
 import { FiX } from 'react-icons/fi'
 
 function SignInButton() {
-  const isUserLoggedIn = false
 
-  return isUserLoggedIn ? (
-    <button type="button"  className={styles.signInButton}>
+  const [ session ] = useSession()
+
+  console.log(session)
+
+  return session ? (
+    <button type="button"  className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub color="#04d361" />
-      Augusto Barreira
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button"  className={styles.signInButton}>
+    <button type="button"  className={styles.signInButton} onClick={() => signIn("github")}>
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,10 +8,30 @@ interface SubscribeButtonProps {
 }
 
 function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [ session ] = useSession()
+
+  async function handleSubscribe() {
+    if(!session){
+      signIn('github')
+      return;
+    }
+
+    try {
+      const { data } = await api.post('subscribe')
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId: data.sessionId})
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subScribeBUtton}
+      onClick={handleSubscribe}
     > 
       Subscribe now
     </button>
